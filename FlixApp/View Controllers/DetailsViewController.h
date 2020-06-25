3c2edff663e1cd862ca421aba4fe809e72a973ed
Monica Bui <monicahb@fb.com>
2020-06-25 10:20:13 -0700

    Implemented basic detail view functionality

---
+++ FlixApp/View Controllers/DetailsViewController.h
@@ -0,0 +1,20 @@
+//
+//  DetailsViewController.h
+//  FlixApp
+//
+//  Created by Monica Bui on 6/24/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+/// public property for movie to display
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- FlixApp/View Controllers/MoviesViewController.m
+++ FlixApp/View Controllers/MoviesViewController.m
@@ -9,6 +9,7 @@
 #import "MoviesViewController.h"
 #import "MovieCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "DetailsViewController.h"
 
 // Step 2: Configure controller to implement two interfaces the table view expects (data source and delegate)
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
@@ -85,14 +86,20 @@
     return cell;
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    detailsViewController.movie = movie;
 }
-*/
+
 
 @end
