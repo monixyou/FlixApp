74763a10f17ecc78b3ad5a7a81acc918ef19efbf
Monica Bui <monicahb@fb.com>
2020-06-26 18:56:08 -0700

    Fixed bug to bring up correct details view with search bar

--- FlixApp/View Controllers/MoviesViewController.m
+++ FlixApp/View Controllers/MoviesViewController.m
@@ -150,7 +150,7 @@
     
     UITableViewCell *tappedCell = sender;
     NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
-    NSDictionary *movie = self.movies[indexPath.row];
+    NSDictionary *movie = self.filteredData[indexPath.row];
     
     DetailsViewController *detailsViewController = [segue destinationViewController];
     detailsViewController.movie = movie;
